979a0ceb5a50cffc6361f9d69f476d8a50cd61ab
ramondeallima <ramondeallima@gmail.com>
2019-09-10 10:24:16 -0300

    Estrutura de teste ainda sem auxílio de frameworks de testes.

--- Alura.LeilaoOnline/Alura.LeilaoOnline.ConsoleApp/Program.cs
+++ Alura.LeilaoOnline/Alura.LeilaoOnline.ConsoleApp/Program.cs
@@ -1,12 +1,74 @@
-﻿using System;
+﻿using Alura.LeilaoOnline.Core;
+using System;
 
 namespace Alura.LeilaoOnline.ConsoleApp
 {
     class Program
     {
+        private static void Verifica(double ValorEsperado, double ValorObtido)
+        {
+            var cor = Console.ForegroundColor;
+
+            if (ValorObtido >= ValorEsperado)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("TESTE OK!");
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(
+                    $"TESTE FALHOU! VALOR ESPERADO: {ValorEsperado}, VALOR OBTIDO:{ValorObtido}");
+            }
+
+            Console.ForegroundColor = cor;
+        }
+        private static void LeilaoComVariosLances()
+        {
+            //Arranje - cenário
+            var leilao = new Leilao("Van Gogh");
+            var fulano = new Interessada("Funano", leilao);
+            var maria = new Interessada("Maria", leilao);
+
+            leilao.RecebeLance(fulano, 800);
+            leilao.RecebeLance(maria, 900);
+            leilao.RecebeLance(fulano, 1000);
+            leilao.RecebeLance(maria, 990);
+            leilao.RecebeLance(maria, 1001);
+
+
+            //Act - método sob teste
+            leilao.TerminaPregao();
+
+            //Assert 
+            var valorEsperado = 5000;
+            var valorObtido = leilao.Ganhador.Valor;
+
+            Verifica(valorEsperado, valorObtido);
+        }
+        private static void LeilaoComApenasUmLance()
+        {
+            //Arranje - cenário
+            var leilao = new Leilao("Van Gogh");
+            var fulano = new Interessada("Funano", leilao);
+
+            leilao.RecebeLance(fulano, 800);
+
+            //Act - método sob teste
+            leilao.TerminaPregao();
+
+            //Assert 
+            var valorEsperado = 800;
+            var valorObtido = leilao.Ganhador.Valor;
+
+            Verifica(valorEsperado, valorObtido);
+        }
+
         static void Main()
         {
-            Console.WriteLine("Hello World!");
+            LeilaoComVariosLances();
+            LeilaoComApenasUmLance();
+
         }
     }
 }
