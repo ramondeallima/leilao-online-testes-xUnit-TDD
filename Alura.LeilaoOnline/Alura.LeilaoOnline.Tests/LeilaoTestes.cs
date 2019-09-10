3e8bba6dcd20b80949f02f246db6ce908ede4f15
ramondeallima <ramondeallima@gmail.com>
2019-09-10 12:55:27 -0300

    Teorizando com xUnit [Theory] e eliminando repetição de testes desnecessários

--- Alura.LeilaoOnline/Alura.LeilaoOnline.Tests/LeilaoTestes.cs
+++ Alura.LeilaoOnline/Alura.LeilaoOnline.Tests/LeilaoTestes.cs
@@ -6,83 +6,30 @@ namespace Alura.LeilaoOnline.Tests
     // A classe de testes deve sempre ser pública
     public class LeilaoTestes
     {
-        [Fact]
-        public void LeilaoComTresClientes()
-        {
-            //Arranje - cenário
-            //Dado leilão pelo menos um lance
-            var leilao = new Leilao("Van Gogh");
-            var fulano = new Interessada("Funano", leilao);
-            var maria = new Interessada("Maria", leilao);
-            var beltrano = new Interessada("Beltrano", leilao);
-
-            leilao.RecebeLance(fulano, 800);
-            leilao.RecebeLance(maria, 900);
-            leilao.RecebeLance(fulano, 1000);
-            leilao.RecebeLance(maria, 990);
-            leilao.RecebeLance(beltrano, 1400);
-
-
-
-            //Act - método sob teste
-            //Quando o pregão/leilão termina
-            leilao.TerminaPregao();
-
-            //Assert 
-            //Então o valor esperado é o maior valor dado
-            //e o cliente ganhador é o que deu o maior lance
-            var valorEsperado = 1400;
-            var valorObtido = leilao.Ganhador.Valor;
-            Assert.Equal(beltrano, leilao.Ganhador.Cliente);
-
-            Assert.Equal(valorEsperado, valorObtido);
-        }
-
-        [Fact]
-        public void LeilaoComLancesOrdenadosPorValor()
-        {
-            //Arranje - cenário
-            var leilao = new Leilao("Van Gogh");
-            var fulano = new Interessada("Funano", leilao);
-            var maria = new Interessada("Maria", leilao);
-
-            leilao.RecebeLance(fulano, 800);
-            leilao.RecebeLance(maria, 900);
-            leilao.RecebeLance(maria, 990);
-            leilao.RecebeLance(fulano, 1000);
-
-
-            //Act - método sob teste
-            leilao.TerminaPregao();
-
-            //Assert 
-            var valorEsperado = 1000;
-            var valorObtido = leilao.Ganhador.Valor;
-
-            Assert.Equal(valorEsperado, valorObtido);
-        }
-
 
         // Anotação necessária para que o framework reconheça o método como um teste de unidade.
-        [Fact]
-        public void LeilaoComVariosLances()
+        [Theory]
+        [InlineData(1000, new double[] { 800, 900, 1000, 1200 })]
+        [InlineData(1000, new double[] { 800, 900, 1000, 990 })]
+        [InlineData(800, new double[] { 800 })]
+        public void LeilaoComVariosLances(
+            double valorEsperado, 
+            double[] ofertas)
         {
             //Arranje - cenário
             var leilao = new Leilao("Van Gogh");
             var fulano = new Interessada("Funano", leilao);
             var maria = new Interessada("Maria", leilao);
 
-            leilao.RecebeLance(fulano, 800);
-            leilao.RecebeLance(maria, 900);
-            leilao.RecebeLance(fulano, 1000);
-            leilao.RecebeLance(maria, 990);
-
+            foreach (var valor in ofertas)
+            {
+                leilao.RecebeLance(fulano, valor);
+            }
 
             //Act - método sob teste
             leilao.TerminaPregao();
 
             //Assert 
-            var valorEsperado = 1000;
             var valorObtido = leilao.Ganhador.Valor;
 
             Assert.Equal(valorEsperado, valorObtido);
@@ -99,26 +46,6 @@ namespace Alura.LeilaoOnline.Tests
 
             //Assert 
             var valorEsperado = 0;
-            var valorObtido = leilao.Ganhador.Valor;
-
-            Assert.Equal(valorEsperado, valorObtido);
-        }
-
-
-        [Fact]
-        public void LeilaoComApenasUmLance()
-        {
-            //Arranje - cenário
-            var leilao = new Leilao("Van Gogh");
-            var fulano = new Interessada("Funano", leilao);
-
-            leilao.RecebeLance(fulano, 800);
-
-            //Act - método sob teste
-            leilao.TerminaPregao();
-
-            //Assert 
-            var valorEsperado = 800;
             var valorObtido = leilao.Ganhador.Valor;
 
             Assert.Equal(valorEsperado, valorObtido);
